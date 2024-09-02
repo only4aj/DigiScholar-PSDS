1b403cc3bbe644f6734d44159762bc01861a6f02
deepu0508 <05deepu08@gmail.com>
2024-09-02 21:52:48 +0530

    env file setup

--- PSDS/views.py
+++ PSDS/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-
+from dotenv import load_dotenv
+import os
 # Create your views here.
 
 
 def index(request):
-    # return render(request , "index.html")
-    pass
+    # Password Load from .env file
+    load_dotenv()
+    return render(request , "index.html",{"key":os.environ.get("PASSWORD")})
+    # pass
 
 
 def login(request):
