574b1680c5455df0353787dcf4f86fecc0f734dc
only4aj <onlyforanshjuneja@gmail.com>
2024-09-02 21:30:59 +0530

    template and static

--- PSDS/models.py
+++ PSDS/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class DocumentStorage(models.Model):
+    pass
+
+
+class BankInfo(models.Model):
+    pass
--- PSDS/views.py
+++ PSDS/views.py
@@ -6,3 +6,13 @@ from django.shortcuts import render
 def index(request):
     # return render(request , "index.html")
     pass
+
+
+def login(request):
+    pass
+
+def sag_bureau(request):
+    pass
+
+def finance_bureau(request):
+    pass
