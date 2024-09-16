4e64ea722ac43c8420dea64625873ec356120576
deepu0508 <05deepu08@gmail.com>
2024-09-16 15:55:50 +0530

    error handling

--- PSDS/views.py
+++ PSDS/views.py
@@ -22,11 +22,13 @@ def scholarship(request):
 def scholarship_form(request):
     return render(request, "scholarship_form.html")
 
+
 # Encryption Function
 def encryptionData(data):
     load_dotenv()
     PKey = getattr(settings, "PUBLICKEY")
     return rsa.encryptionData(PKey, os.environ.get("PKEY"), data)
+
 
 def docs_upload(request):
     studentName = request.POST.get("name", "")
@@ -74,35 +76,53 @@ def docs_upload(request):
         and caccount_number
         and ifsc
     ):
-        return render(request, "scholarship_form.html",{"message":"Please fill all details!"})
-    
-    
-    ency_Name  =  encryptionData(studentName)
-    ency_fatherName = encryptionData(fatherName)
-    ency_motherName = encryptionData(motherName)
-    ency_gender = encryptionData(gender)
-    ency_email = encryptionData(email)
-    ency_phone = encryptionData(phone)
-    ency_dob = encryptionData(dob)
-    ency_nationality  =  encryptionData(nationality)
-    ency_address  =  encryptionData(permanent_address)
-    ency_institution  =  encryptionData(institution)
-    ency_course  =  encryptionData(course)
-    ency_currentYear  =  encryptionData(current_year)
-    ency_courseType  =  encryptionData(course_type)
-    ency_tenthMarks  =  encryptionData(tenth_marks)
-    ency_tenthPer  =  encryptionData(tenth_percentage)
-    ency_twelthMarks  =  encryptionData(twelth_marks)
-    ency_twelthPer  =  encryptionData(twelth_percentage)
-    ency_bank  =  encryptionData(bank)
-    ency_accNo  =  encryptionData(account_number)
-    ency_caccNo  =  encryptionData(caccount_number)
-    ency_ifsc  =  encryptionData(ifsc)
-    
+        return render(
+            request, "scholarship_form.html", {"message": "Please fill all details!"}
+        )
+
+    try:
+        ency_Name = encryptionData(studentName)
+        ency_fatherName = encryptionData(fatherName)
+        ency_motherName = encryptionData(motherName)
+        ency_gender = encryptionData(gender)
+        ency_email = encryptionData(email)
+        ency_phone = encryptionData(phone)
+        ency_dob = encryptionData(dob)
+        ency_nationality = encryptionData(nationality)
+        ency_address = encryptionData(permanent_address)
+        ency_institution = encryptionData(institution)
+        ency_course = encryptionData(course)
+        ency_currentYear = encryptionData(current_year)
+        ency_courseType = encryptionData(course_type)
+        ency_tenthMarks = encryptionData(tenth_marks)
+        ency_tenthPer = encryptionData(tenth_percentage)
+        ency_twelthMarks = encryptionData(twelth_marks)
+        ency_twelthPer = encryptionData(twelth_percentage)
+        ency_bank = encryptionData(bank)
+        ency_accNo = encryptionData(account_number)
+        ency_caccNo = encryptionData(caccount_number)
+        ency_ifsc = encryptionData(ifsc)
+
+        print(ency_institution)
+        print(ency_nationality)
+        print(ency_Name)
+        print(ency_fatherName)
+        print(ency_motherName)
+        print(ency_gender)
+        print(ency_caccNo)
+        print(ency_course)
+        print(ency_tenthMarks)
+        print(ency_email)
+        print(ency_phone)
+    except Exception:
+        print("Some Error Occured!")
     return render(request, "docsupload.html")
 
+
 def register(request):
-    return render(request,"register.html")
+    return render(request, "register.html")
+
+
 def sag_bureau(request):
     pass
 
--- static/js/scholarship_form.js
+++ static/js/scholarship_form.js
@@ -5,78 +5,104 @@ document.getElementById("date").min = `${new Date().getFullYear() - PREVIOUS_YEA
 
 // Next Button Disabled Function
 const btnHide = (check) => {
-    check ?
-        document.querySelector("#btn").disabled = true :
-        document.querySelector("#btn").disabled = false;
+    try {
+        check ?
+            document.querySelector("#btn").disabled = true :
+            document.querySelector("#btn").disabled = false;
+    } catch (error) {
+        console.log(error)
+    }
 };
 
 // Phone Number validation Check
 const checkPhone = () => {
-    let value = document.querySelector("#phone").value;
-    if ((value.length !== 12 && value !== "") || !value.startsWith("91")) {
-        document.querySelector("#warning1").innerText = "Phone Number is not valid!";
-        btnHide(true);
-    } else {
-        document.querySelector("#warning1").innerText = "";
+    try {
+        let value = document.querySelector("#phone").value;
+        if ((value.length !== 12 && value !== "") || !value.startsWith("91")) {
+            document.querySelector("#warning1").innerText = "Phone Number is not valid!";
+            btnHide(true);
+        } else {
+            document.querySelector("#warning1").innerText = "";
+        }
+    } catch (error) {
+        console.log(error)
     }
 };
 
 // Check account number is same with confirm account number or not
 const checkAccNo = () => {
-    let value1 = document.getElementById("account_number").value;
-    let value2 = document.getElementById("caccount_number").value;
-    if (value1 !== value2 && value1 !== "" && value2 !== "") {
-        document.querySelector("#warning2").innerText = "Account Number is not matched!";
-        document.querySelector("#warning3").innerText = "Account Number is not matched!";
-        btnHide(true);
-    } else {
-        document.querySelector("#warning2").innerText = "";
-        document.querySelector("#warning3").innerText = "";
+    try {
+        let value1 = document.getElementById("account_number").value;
+        let value2 = document.getElementById("caccount_number").value;
+        if (value1 !== value2 && value1 !== "" && value2 !== "") {
+            document.querySelector("#warning2").innerText = "Account Number is not matched!";
+            document.querySelector("#warning3").innerText = "Account Number is not matched!";
+            btnHide(true);
+        } else {
+            document.querySelector("#warning2").innerText = "";
+            document.querySelector("#warning3").innerText = "";
+        }
+    } catch (error) {
+        console.log(error)
     }
 };
 
 // Check precentage value is valid or not
 const checkPercentage = (id, warningId) => {
-    let value = document.getElementById(id).value;
-    if ((parseInt(value) <= 100 && parseInt(value) >= 0) || value === "") {
-        document.getElementById(warningId).innerText = "";
-    } else {
-        document.getElementById(warningId).innerText = "Percentage is invalid!";
-        btnHide(true);
+    try {
+        let value = document.getElementById(id).value;
+        if ((parseInt(value) <= 100 && parseInt(value) >= 0) || value === "") {
+            document.getElementById(warningId).innerText = "";
+        } else {
+            document.getElementById(warningId).innerText = "Percentage is invalid!";
+            btnHide(true);
+        }
+    } catch (error) {
+        console.log(error)
     }
 };
 
 // Check Complete Form fill or not
 const checkFillForm = () => {
-    let inputGroup = Array.from(document.querySelectorAll(".form-control"));
-    let check = false;
-    for (let i in inputGroup) {
-        if (inputGroup[i].value === "") {
-            check = true;
-            break;
-        } else {
-            check = false;
+    try {
+        let inputGroup = Array.from(document.querySelectorAll(".form-control"));
+        let check = false;
+        for (let i in inputGroup) {
+            if (inputGroup[i].value === "") {
+                check = true;
+                break;
+            } else {
+                check = false;
+            }
         }
+        btnHide(check);
+    } catch (error) {
+        console.log(error)
     }
-    btnHide(check);
 };
 
 // Extra checker for checking  error in every stage
 const checker = () => {
-    let inputgroup = Array.from(document.querySelectorAll(".form-control"));
-    for (let i in inputgroup) {
-        if (inputgroup[i].value !== "" && inputgroup[i].id === "phone") {
-            checkPhone();
+    try {
+        let inputgroup = Array.from(document.querySelectorAll(".form-control"));
+        for (let i in inputgroup) {
+            if (inputgroup[i].value !== "" && inputgroup[i].id === "phone") {
+                checkPhone();
+            }
+            if (inputgroup[i].value !== "" && inputgroup[i].id === "10percentage") {
+                checkPercentage("10percentage", "warning4");
+            }
+            if (inputgroup[i].value !== "" && inputgroup[i].id === "12percentage") {
+                checkPercentage('12percentage', 'warning5');
+            }
+            if (inputgroup[i].value !== "" && inputgroup[i].id === "account_number") {
+                checkAccNo();
+            }
         }
-        if (inputgroup[i].value !== "" && inputgroup[i].id === "10percentage") {
-            checkPercentage("10percentage", "warning4");
-        }
-        if (inputgroup[i].value !== "" && inputgroup[i].id === "12percentage") {
-            checkPercentage('12percentage', 'warning5');
-        }
-        if (inputgroup[i].value !== "" && inputgroup[i].id === "account_number") {
-            checkAccNo();
-        }
+    } catch (error) {
+        console.log(error)
     }
 
 }
+// Automatic check
+checker()
